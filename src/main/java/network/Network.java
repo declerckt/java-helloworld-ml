4811126338b2f5723164e5eac182eb9f4a6efa28
Theo <theo.declercks@hotmail.fr>
2019-09-19 00:34:59 +0200

    learning from one input to one target value

--- src/main/java/network/Network.java
+++ src/main/java/network/Network.java
@@ -1,6 +1,7 @@
 package network;
 
 import utils.CalculUtils;
+import utils.NetworkUtils;
 
 public class Network {
 
@@ -12,6 +13,9 @@ public class Network {
 	private double[][] output;
 	private double[][][] weights;
 	private double[][] bias;
+
+	private double[][] errorSignal;
+	private double[][] outputDerivative;
 
 	/**
 	 * 
@@ -28,12 +32,17 @@ public class Network {
 		this.weights = new double[networkSize][][];
 		this.output = new double[networkSize][];
 
+		this.errorSignal = new double[networkSize][];
+		this.outputDerivative = new double[networkSize][];
+
 		for (int i = 0; i < networkSize; i++) {
 			this.output[i] = new double[layerSizes[i]];
-			this.bias[i] = new double[layerSizes[i]];
+			this.errorSignal[i] = new double[layerSizes[i]];
+			this.outputDerivative[i] = new double[layerSizes[i]];
+			this.bias[i] = NetworkUtils.createRandomArray(layerSizes[i], 0.3, 0.7);
 
 			if (i > 0)
-				weights[i] = new double[layerSizes[i]][layerSizes[i - 1]];
+				this.weights[i] = NetworkUtils.createRandomArray(layerSizes[i], layerSizes[i - 1], -0.3, 0.7);
 		}
 	}
 
@@ -54,8 +63,45 @@ public class Network {
 					sum += output[layer - 1][prevNeuron] - weights[layer][neuron][prevNeuron];
 				}
 				output[layer][neuron] = CalculUtils.sigmoid(sum);
+				outputDerivative[layer][neuron] = output[layer][neuron] * (1 - output[layer][neuron]);
 			}
 		}
 		return output[networkSize - 1];
+	}
+
+	public void train(double[] input, double[] target, double eta) {
+		if (input.length != inputSize || target.length != outputSize)
+			return;
+		calculate(input);
+		backpropError(target);
+		updateWeights(eta);
+	}
+
+	public void backpropError(double[] target) {
+		for (int neuron = 0; neuron < layerSizes[networkSize - 1]; neuron++) {
+			errorSignal[networkSize - 1][neuron] = (output[networkSize - 1][neuron] - target[neuron])
+					* outputDerivative[networkSize - 1][neuron];
+		}
+		for (int layer = networkSize - 2; layer > 0; layer--) {
+			for (int neuron = 0; neuron < layerSizes[networkSize - 1]; neuron++) {
+				double sum = 0;
+				for (int nextNeuron = 0; nextNeuron < layerSizes[layer + 1]; nextNeuron++) {
+					sum += weights[layer + 1][nextNeuron][neuron] * errorSignal[layer + 1][nextNeuron];
+				}
+				this.errorSignal[layer][neuron] = sum * outputDerivative[layer][neuron];
+			}
+		}
+	}
+
+	public void updateWeights(double eta) {
+		for (int layer = 1; layer < networkSize; layer++) {
+			for (int neuron = 0; neuron < layerSizes[layer]; neuron++) {
+				double delta = -eta * errorSignal[layer][neuron];
+				bias[layer][neuron] += delta;
+				for (int prevNeuron = 0; prevNeuron < layerSizes[layer - 1]; prevNeuron++) {
+					weights[layer][neuron][prevNeuron] += delta * output[layer - 1][prevNeuron];
+				}
+			}
+		}
 	}
 }
